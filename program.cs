ac39b2a6888972ebeb7aea45f7f06cbd58f957f9
CMD <gautamkailashsingh@gmail.com>
2020-09-30 13:55:17 +0530

    [Gautam] Added monthly wages feature

--- program.cs
+++ program.cs
@@ -4,22 +4,27 @@ class program
 	{
 		Console.WriteLine("Welcome to Employee Wage Coputation Program");
 		Random ran = new Random();
-		int atten = ran.Next(0,3);
-		int wage=20, hour=8;
-		switch(atten)
+		int totalWage=0;
+		for(int i=0;i<20;i++)
 		{
-			case 0:
-				hour=0;
-				Console.WriteLine("Employee is absent");
-				break;
-			case 1:
-				hour=4;
-				Console.WriteLine("Employee is prersent for part time");
-				break;
-			default:
-				Console.WriteLine("Employee is present for full time");
-				break;`
+			int atten = ran.Next(0,3);
+			int wage=20, hour=8;
+			switch(atten)
+			{
+				case 0:
+					hour=0;
+					Console.WriteLine("Employee is absent");
+					break;
+				case 1:
+					hour=4;
+					Console.WriteLine("Employee is prersent for part time");
+					break;
+				default:
+					Console.WriteLine("Employee is present for full time");
+					break;
+			}
+			totalWage+=(wage*hour);
 		}
-		Console.WriteLine("Daily Employee Wage: "+(wage*hour));
+		Console.WriteLine("Monthly Employee Wages: "+totalWage);
 	}
 }
