6b8fd4e6a2fc4b944c91dcc0b2b3b9e791cbc783
CMD <gautamkailashsingh@gmail.com>
2020-09-30 13:27:55 +0530

    [Gautam] Added daily wage calculation feature

--- program.cs
+++ program.cs
@@ -5,9 +5,14 @@ class program
 		Console.WriteLine("Welcome to Employee Wage Coputation Program");
 		Random ran = new Random();
 		int atten = ran.Next(0,2);
+		int wage=20, hour=8;
 		if(atten == 0)
+		{
+			hour=0;
 			Console.WriteLine("Employee is absent");
+		}
 		else
 			Console.WriteLine("Employee is prersent");
+		Console.WriteLine("Daily Employee Wage: "+(wage*hour));
 	}
 }
