e2faa159c31a6dd85cd0a13ecd8e364e98d9ac18
CMD <gautamkailashsingh@gmail.com>
2020-09-30 13:34:47 +0530

    [Gautam] Added part time employee feature

--- program.cs
+++ program.cs
@@ -4,15 +4,20 @@ class program
 	{
 		Console.WriteLine("Welcome to Employee Wage Coputation Program");
 		Random ran = new Random();
-		int atten = ran.Next(0,2);
+		int atten = ran.Next(0,3);
 		int wage=20, hour=8;
 		if(atten == 0)
 		{
 			hour=0;
 			Console.WriteLine("Employee is absent");
 		}
+		else if(atten == 1)
+		{
+			hour=4;
+			Console.WriteLine("Employee is prersent for part time");
+		}
 		else
-			Console.WriteLine("Employee is prersent");
+			Console.WriteLine("Employee is present for full time");
 		Console.WriteLine("Daily Employee Wage: "+(wage*hour));
 	}
 }
